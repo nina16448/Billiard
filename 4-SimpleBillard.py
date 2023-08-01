a9eb7b2c776b8d4524a72d1c6cc512f25a133969
nina16448 <nina164488@gmail.com>
2023-08-02 00:04:55 +0800

    7.5FPS

--- 4-SimpleBillard.py
+++ 4-SimpleBillard.py
@@ -5,10 +5,6 @@ from math import *
 from time import *
 import time
 import matplotlib.pyplot as plt
-
-# import pymunk
-
-# import pymunk.pygame_util
 import numpy.polynomial.polynomial as poly
 from random import random
 import os.path
@@ -37,41 +33,13 @@ cv2.setWindowProperty(
     "Billard", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN
 )  # , cv2.WINDOW_NORMAL, cv2.WINDOW_NORMAL) #
 
-if os.path.isfile("debug.mp4"):
-    cap = cv2.VideoCapture("debug.mp4")
-else:
-    cap = cv2.VideoCapture(camera_number)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    cap.set(cv2.CAP_PROP_FPS, 30)
-    print("fps: ", cap.get(cv2.CAP_PROP_FPS))
-# cap = cv2.VideoCapture('output.avi')
 
-############### Billard ###############
-"""
-cap = cv2.VideoCapture(camera_number, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(camera_number)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-cap.set(cv2.CAP_PROP_FPS, 25)
-fps = int(cap.get(5))
-print("fps:", fps)
+cap.set(cv2.CAP_PROP_FPS, 30)
+print("fps: ", cap.get(cv2.CAP_PROP_FPS))
 
-
-ret, frame = cap.read()
-
-matrix=m_projector2camera.dot(m_camera2screen)
-
-while True:
-    ret, frame = cap.read()
-    frame = cv2.warpPerspective(frame, matrix, (1920,1080), flags=cv2.INTER_LINEAR)
-    cv2.imshow('Billard', frame)
-    k = cv2.waitKey(1) & 0xff
-    if k == 27:
-        break
-
-        
-cv2.destroyAllWindows()
-cap.release()"""
 
 ret, frame = cap.read()
 l, h, c = frame.shape
@@ -79,34 +47,11 @@ cadre = (1920, 1080)
 print(l, h)
 
 
-"""screen = pygame.display.set_mode(cadre)
-#screen = pygame.display.set_mode(cadre, pygame.FULLSCREEN)
-draw_options = pymunk.pygame_util.DrawOptions(screen)
-"""
-
 flagg = False
 clean_time = -3
 put_time = 0
 POSlist = []
 ipPOSlist = []
-##### Moteur Physique #####
-# space = pymunk.Space()
-# space.gravity = (0, 0)
-
-# # Edge of billard:
-# body = space.static_body
-# body.position = (0, 0)
-# # lEdges = [
-#     pymunk.Segment(body, (0, 0), (cadre[0], 0), 10),
-#     pymunk.Segment(body, (cadre[0], 0), (cadre[0], cadre[1]), 1),
-#     pymunk.Segment(body, (cadre[0], cadre[1]), (0, cadre[1]), 1),
-#     pymunk.Segment(body, (0, cadre[1]), (0, 0), 1),
-# ]
-# for edge in lEdges:
-#     edge.elasticity = 0.95
-#     edge.friction = 0  ## 0.1
-
-# space.add(*lEdges)
 
 
 class Ball:
@@ -130,72 +75,22 @@ class Ball:
         self.lVitesse = [[t, 0, 0, 0]]  # [[t, dx, dy, v]]
         self.lVitesse_prediction = [[t_physic_engine, 0, 0, 0]]
         self.polynome_distance = poly.Polynomial([0])
-
         self.debut_simulation = 0
-
         self.lChemin = []
         self.lVt1 = []
         self.lVt2 = []
-
         self.lTest = []
-
         self.lAngle = []
-
         self.lAcc = [[t, 0]]
-
         self.distance = 0
-
         self.std = []
-
         self.id = min(Ball.sId_available)
         Ball.sId_available.remove(self.id)
-
         self.explosion_frame = 0
         self.debut_simulation = time.time()
-
         self.movement = 4
         self.prediction_run = False
-
-        # inertia = pymunk.moment_for_circle(Ball.mass, 0, Ball.radius, (0, 0))
-        # body = pymunk.Body(Ball.mass, inertia)
-        # body.position = (x, y)
-
-        # shape = pymunk.Circle(body, Ball.radius, (0, 0))
-        # shape.elasticity = 1  # 0.95
-        # shape.friction = 0.1  # 0.9
-        # space.add(body, shape)
-
-        # body.velocity_func = self.static_friction
-
-        # self.shape = shape
-        # self.body = body
         self.creation_time = time.time()
-
-    # def static_friction(self, body, gravity, damping, dt):
-    #     pymunk.Body.update_velocity(body, gravity, damping, dt)
-
-    #     if self.prediction_run:
-    #         if round(body.velocity.length, 3) < 0.01:
-    #             body.velocity = body.velocity * 0
-    #         else:
-    #             if len(ball.lVitesse) > 1:
-    #                 v = float(self.lVitesse[-1][3])
-    #             else:
-    #                 v = 0.0
-
-    #             body.velocity = (
-    #                 body.velocity
-    #                 / body.velocity.length
-    #                 * v
-    #                 * 2
-    #                 * float(np.exp(-self.debut_simulation / 4))
-    #             )
-    #             # body.velocity = body.velocity/body.velocity.length*max(ball.lAcc[-1][1]*dt, v)**1.1
-    #     else:
-    #         # body.velocity = body.velocity - body.velocity/body.velocity.length * 30 #* float(np.exp(-self.debut_simulation/4))
-    #         pass
-
-    #     self.debut_simulation += dt
 
     def __str__(self):
         return f"Ball ID: {self.id}"
@@ -203,8 +98,8 @@ class Ball:
     def __del__(self):
         global space
         Ball.sId_available.add(self.id)
-        # space.remove(self.body, self.shape)
-        Ball.lBall.remove(self)
+        if self in Ball.lBall:
+            Ball.lBall.remove(self)
         del self
 
     def interpolation(self, dt):
@@ -247,14 +142,6 @@ class Ball:
                 np.average(aAverage, weights=np.exp((aAverage[:, 0] - t) / 0.1), axis=0)
             )
         ]  # [[t, x, y], ...]
-
-        # if self.lPos_prediction[-1][0] != t_physic_engine:
-        #     self.lPos_prediction += [[t_physic_engine] + list(self.body.position)]
-        #     self.lVitesse_prediction += [
-        #         [t_physic_engine]
-        #         + list(self.body.velocity)
-        #         + [self.body.velocity.length]
-        #     ]
 
         if len(self.lPos) < ball.memory:
             return
@@ -349,16 +236,13 @@ class Ball:
                 dt = (time.time() - t_prediction) * 7
                 px = min(max(x + dx * v * dt, 1), 1919)
                 py = min(max(y + dy * v * dt, 1), 1079)
-                # self.body.position = pymunk.Vec2d(px, py)
-                # self.body.velocity = pymunk.Vec2d(dx, dy) * 1000
+
                 scale = 100
-                # self.body.apply_impulse_at_local_point((dx*scale, dy*scale))
+
                 self.prediction_run = True
                 self.debut_simulation = 0
 
         if v == 0:
-            # self.body.position = pymunk.Vec2d(x, y)
-            # self.body.velocity = pymunk.Vec2d(0, 0)
             self.prediction_run = False
 
             self.lPos_prediction = [[t_physic_engine, x, y]]
@@ -417,17 +301,6 @@ class Ball:
             Ball(t, *detected)
 
 
-"""
-Vitesse maximale boule: 12,5 m/s
-FPS: 30 image/s
-Largeur billard: 3,6 m
-Largeur image: 1920 px
-
-RayonMinimal = VitesseMaximaleBoule / FPS / LargeurBillard * LargeurImage
- = 12,5 / 30 / 3,6 * 1920
- = 
-"""
-
 _, frame = cap.read()
 
 prevframe = frame[:, :, :]  # frame[:,:,2]    #First frame
@@ -456,6 +329,7 @@ rec_time = -5
 
 maxx = 0
 prev_M = []
+
 while True:
     t_frame = time.time()
     ret, frame = cap.read()
@@ -523,7 +397,7 @@ while True:
         # if maxx < circularity:
         #     maxx = circularity
         #     print(maxx)
-        print("circularity", circularity)
+        # print("circularity", circularity)
         if circularity < 0.5:
             continue
 
@@ -554,10 +428,11 @@ while True:
         aPos_y = 5.548624
         aPos_z = (7.2 - float(x / 1919.0 * 14.4)) * 1
 
-        if flagg == False and t - clean_time > 3 and v == 0:
+        if flagg == False and v == 0:
             if put_time == 0:
                 put_time = t
             if t - put_time > 5:
+                POSlist = []
                 print("New Position: ", ball)
                 print(aPos_x, ", ", aPos_y, ", ", aPos_z)
                 Posdata = {
@@ -613,70 +488,6 @@ while True:
             -1,
         )
 
-        # if v > 0:
-        #     cv2.arrowedLine(
-        #         newframe,
-        #         (int(x), int(y)),
-        #         (int(x + vx * zoom), int(y + vy * zoom)),
-        #         (255, 255, 255),
-        #         10,
-        #     )
-
-        #     if len(ball.lChemin) >= 2:
-        #         pts = np.array(ball.lChemin + ball.lVt1[::-1], np.int32).reshape(
-        #             (-1, 1, 2)
-        #         )
-        #         # cv2.fillPoly(newframe, [pts], (110, 0, 0))
-        #         chemin = np.array(ball.lChemin + [[x, y]], np.int32).reshape((-1, 1, 2))
-        #         vdx, vdy = np.array([x, y]) - np.array(ball.lChemin[-1])
-        #         vitesse = np.array(
-        #             ball.lVt1
-        #             + [[ball.lVt1[-1][0] + vdx, ball.lVt1[-1][1] + vdy]]
-        #             + [[ball.lVt2[-1][0] + vdx, ball.lVt2[-1][1] + vdy]]
-        #             + ball.lVt2[::-1],
-        #             np.int32,
-        #         ).reshape((-1, 1, 2))
-        #         cv2.fillPoly(newframe, [vitesse], (255, 0, 0))
-        #         cv2.polylines(newframe, [vitesse], True, (0, 0, 255), 2)
-
-        #         # acc = np.array(ball.lAcceleration_tangencielle, np.int32).reshape((-1, 1, 2))
-        #         # cv2.polylines(newframe, [vitesse], False, (0, 110, 0), 5)
-        #         # cv2.polylines(newframe, [acc], False, (0, 0, 110), 5)
-        #         cv2.polylines(newframe, [chemin], False, (255, 255, 255), 2)
-        #         vu = np.array(ball.lVt1[0]) - np.array(ball.lChemin[0])
-        #         vu /= np.linalg.norm(vu)
-        #         s = 55
-        #         cv2.arrowedLine(
-        #             newframe,
-        #             tuple(map(int, ball.lChemin[0])),
-        #             tuple(map(int, np.array(ball.lChemin[0]) + vu * s)),
-        #             (255, 255, 255),
-        #             2,
-        #         )
-        #         cv2.arrowedLine(
-        #             newframe,
-        #             tuple(map(int, ball.lChemin[0])),
-        #             tuple(map(int, np.array(ball.lChemin[0]) - vu * s)),
-        #             (255, 255, 255),
-        #             2,
-        #         )
-
-        #         cv2.putText(
-        #             newframe,
-        #             "Speed(px/s)",
-        #             tuple(map(int, ball.lChemin[1])),
-        #             cv2.FONT_HERSHEY_SIMPLEX,
-        #             1,
-        #             (255, 255, 255),
-        #             2,
-        #             cv2.LINE_AA,
-        #         )
-
-        #         prediction = np.array(
-        #             [[[x, y]] for t, x, y in ball.lPos_prediction], np.int32
-        #         )
-        #         cv2.polylines(newframe, [prediction], False, (0, 255, 0), 4)
-
         font_scale = cv2.getFontScaleFromHeight(cv2.FONT_HERSHEY_SIMPLEX, 30, 3)
         cv2.putText(
             newframe,
@@ -699,135 +510,25 @@ while True:
             cv2.LINE_AA,
         )
 
-        """if explosion:
-            f = explosion_gif[ball.explosion_frame]
-            ball.explosion_frame+=1
-
-            n_frame, l, h, c = explosion_gif.shape
-            nextframe"""
-
-    # cv2.putText(
-    #     newframe,
-    #     "FPS: " + str(int(1 / (time.time() - t_frame))),
-    #     (50, 50),
-    #     cv2.FONT_HERSHEY_SIMPLEX,
-    #     2,
-    #     (255, 255, 255),
-    #     3,
-    #     cv2.LINE_AA,
-    # )
-
-    """cv2.putText(newframe,
-                "Frame: " + str(n_frame),
-                (50, 150),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                2,
-                (255, 255, 255),
-                3,
-                cv2.LINE_AA)"""
-    n_frame += 1
-    """dt = (time.time() - t_prediction)
-    for i in range(10):
-        space.step(dt/10)"""
+    cv2.putText(
+        newframe,
+        "FPS: " + str(int(1 / (time.time() - t_frame))),
+        (50, 50),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        2,
+        (255, 255, 255),
+        3,
+        cv2.LINE_AA,
+    )
 
     step = 1 / 120
     n = int((time.time() - debut_time - t_physic_engine) / step)
-    # for i in range(n):
-    # space.step(step)
-
-    # t_physic_engine += step * n
-
-    # print(space.current_time_step)
-    # t_prediction = time.time()
-
-    # newframe = cv2.warpPerspective(
-    #     newframe, m_projector2camera, (1920, 1080), flags=cv2.INTER_LINEAR
-    # )
 
     cv2.imshow("Billard", newframe)
 
-    # k = cv2.waitKey(int((time.time() - t_frame)*1000)) & 0xff
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
-
-    idBall = k - ord("a")
-    if idBall in [b.id for b in Ball.lBall]:
-        ball = Ball.lBall[idBall]
-
-        fig, axs = plt.subplots(2, 2)
-        lVt = [t for t, vx, vy, v in ball.lVitesse]
-        lVv = [v for t, vx, vy, v in ball.lVitesse]
-        axs[0, 0].plot(lVt, lVv, "o-")
-        axs[0, 0].set(
-            xlabel="Time (s)", ylabel="Speed (px/s)", title="Speed of the ball"
-        )
-        axs[0, 0].legend(loc="best")
-        # axs[0, 0].set_title("Vitesse de la balle")
-
-        a = np.array(ball.lAcc)
-        axs[0, 1].plot(a[:, 0], a[:, 1], "o-")
-        axs[0, 1].set(
-            xlabel="Time (s)",
-            ylabel="Acceleration (px/s²)",
-            title="Acceleration of the ball",
-        )
-        # plt.xlabel("Temp")
-        # plt.ylabel("Accélération")
-        # plt.figure()
-
-        # plt.draw()
-        # plt.pause(0.001)
-
-        lT = [t for t, x, y in ball.lPos]
-        lX = [x for t, x, y in ball.lPos]
-        lY = [y for t, x, y in ball.lPos]
-        lT_prediction = [t for t, x, y in ball.lPos_prediction]
-        lX_prediction = [x for t, x, y in ball.lPos_prediction]
-        lY_prediction = [y for t, x, y in ball.lPos_prediction]
-        axs[1, 0].plot(lX, lY, "o-", label="Ball")
-        axs[1, 0].plot(lX_prediction, lY_prediction, "x-", label="Prediction")
-        axs[1, 0].set(xlabel="X", ylabel="Y", title="Position of the ball")
-        axs[1, 0].legend(loc="best")
-
-        lT = [t for t, a in ball.lAngle]
-        lA = [a for t, a in ball.lAngle]
-        axs[1, 1].plot(lT, lA, "o-")
-        axs[1, 1].set(
-            xlabel="Time(s)",
-            ylabel="Direction of the ball (radians)",
-            title="Direction of the ball",
-        )
-
-        """for t, x, y in zip(lT, lX, lY):
-            plt.annotate(int(t), (x, y))
-        #plt.plot(lX_prediction, lY_prediction, 'o-')
-        for t, x, y in zip(lT_prediction, lX_prediction, lY_prediction):
-            plt.annotate(int(t), (x, y))"""
-        # plt.xlabel("X")
-        # plt.ylabel("Y")
-
-        # plt.draw()
-        # plt.pause(0.001)
-
-        # plt.ion()
-        plt.show()
-        # plt.pause(0.001)
-
-        # plt.ion()
-        # plt.show(block = False)
-        """
-        fig = plt.figure()
-        fig_ax = fig.add_subplot(111)
-        fig_ax.plot(lVtp, lVvp, 'o-')
-        fig_ax.set_title("test")
-        fig_ax.grid(True)
-        fig.show()"""
-
-    """
-    screen.fill((0,0,0))
-    space.debug_draw(draw_options)
-    pygame.display.update()"""
 
 cv2.destroyAllWindows()
 cap.release()
